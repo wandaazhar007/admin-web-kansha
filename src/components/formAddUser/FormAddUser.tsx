7581a520c2ddfe218b5ca0a003daa39f56294385
wanda azhar <wandaazhar@gmail.com>
2023-08-15 23:55:53 -0400

    refactor code

--- src/components/formAddUser/FormAddUser.tsx
+++ src/components/formAddUser/FormAddUser.tsx
@@ -3,7 +3,7 @@ import './formAddUser.scss';
 import { useContext, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { TriggerContext } from '../../context/TriggerContext';
 
 const FormAddUser = ({ setButtonAdd, buttonAdd }: any) => {
@@ -16,7 +16,7 @@ const FormAddUser = ({ setButtonAdd, buttonAdd }: any) => {
   const [preview, setPreview] = useState('');
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
 
   const triggerCon: any = useContext(TriggerContext);
@@ -55,9 +55,7 @@ const FormAddUser = ({ setButtonAdd, buttonAdd }: any) => {
       setImage('');
       setButtonAdd(!buttonAdd)
       triggerCon.trigger();
-      setTimeout(() => {
-        navigate('/users')
-      }, 2500);
+      setIsLoading(true);
     } catch (error: any) {
       if (error.response) {
         setMessage(error.response.data.msg);
