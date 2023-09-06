52ebcbec7d72b2ece0e4faa9342dea32037a671b
wanda azhar <wandaazhar@gmail.com>
2023-09-06 16:36:24 -0400

    refactor code

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -27,7 +27,7 @@ const ProductList: React.FC = () => {
   const active = triggerCon.active;
 
   const getMenu = async () => {
-    const response = await axios.get(`${import.meta.env.VITE_SEARCH_PRODUCT}?search_query=${querySearch}&page=${page}&limit=5`);
+    const response = await axios.get(`${import.meta.env.VITE_SEARCH_PRODUCT}?search_query=${querySearch}&page=${page}&limit=10`);
     // const response = await axios.get(`https://api.kanshamissouri.com/search-products?search_query=${querySearch}&page=${page}&limit=10`);
     setTimeout(() => {
       setMenus(response.data.result);
